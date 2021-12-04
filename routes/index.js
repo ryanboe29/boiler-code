f2b7bdada69d6b6295d0a8abc3152a276dee7c22
Ryan Boeckenstedt <boardgamesrm3@gamil.com>
2021-12-03 18:07:52 -0600

    added post and get route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('You successfully created a GET route!');
+});
+
+router.post('/', function(req, res, next) {
+  let author = req.body.author
+  let hobbies = req.body.hobbies
+  res.send(author);
 });
 
 module.exports = router;
